82020cb8c8053b48d4857234d0c039a7cc111b6d
GeovanaGoncalves <geovanacgoncalves@yahoo.com.br>
2017-01-26 16:06:02 -0200

    Update dashboard.rb

--- lib/rails_admin/config/actions/dashboard.rb
+++ lib/rails_admin/config/actions/dashboard.rb
@@ -15,20 +15,9 @@ module RailsAdmin
         register_instance_option :controller do
           proc do
             @history = @auditing_adapter && @auditing_adapter.latest || []
-            if @action.statistics?
-              @abstract_models = RailsAdmin::Config.visible_models(controller: self).collect(&:abstract_model)
-
-              @most_recent_created = {}
-              @count = {}
-              @max = 0
-              @abstract_models.each do |t|
-                scope = @authorization_adapter && @authorization_adapter.query(:index, t)
-                current_count = t.count({}, scope)
-                @max = current_count > @max ? current_count : @max
-                @count[t.model.name] = current_count
-                next unless t.properties.detect { |c| c.name == :created_at }
-                @most_recent_created[t.model.name] = t.model.last.try(:created_at)
-              end
+            
+            register_instance_option :link_icon do
+              'icon-home'
             end
             render @action.template_name, status: @status_code || :ok
           end
